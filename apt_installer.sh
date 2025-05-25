01b9d531e8759c6eb742a993fe28b59337458465
gowthamgeddam <gowthamgeddam800@gmail.com>
2025-05-25 16:10:14 +0530

    added basic config

---
+++ apt_installer.sh
@@ -0,0 +1,18 @@
+# update packages
+sudo apt update
+sudo apt upgrade -y
+
+# install curl
+sudo apt install curl
+
+# install git
+sudo apt install git
+
+# install compilers/interpreters
+sudo apt install gcc g++ python3
+
+# install browser
+sudo apt install firefox
+
+# install neofetch
+sudo apt install neofetch
---
+++ autorun.sh
@@ -0,0 +1,7 @@
+mv .bash_aliases ~/.bash_aliases
+
+mv .bashrc ~/.bashrc
+
+chmod +x apt_install.sh
+
+./apt_install.sh
